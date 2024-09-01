34be1387abe3a3ce6d70c235fc0522e819cf8062
space-lab-org <91821366+space-lab-org@users.noreply.github.com>
2024-09-01 16:38:25 +0800

    chore: update token

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-
+// this is a first app with nice fabrik to cover - REMEMBER with _
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
